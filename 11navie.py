22fdfe9a7538683a20606a602bf901d52fa0970c
ushayenni <151708074+ushayenni@users.noreply.github.com>
2025-11-09 19:45:00 +0530

    Create 11navie.py

---
+++ 11navie.py
@@ -0,0 +1,14 @@
+from sklearn.datasets import load_iris
+
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, classification_report
+iris = load_iris()
+X = iris.data
+y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+model = GaussianNB()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("\nClassification Report:\n", classification_report(y_test, y_pred))
